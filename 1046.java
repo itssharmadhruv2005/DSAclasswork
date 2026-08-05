e1cab878b829db7fb1e417e0c28f4a33a954df41
itssharmadhruv2005 <dhruvsharmabe2324@gmail.com>
2026-08-05 20:26:25 +0530

    done this question

---
+++ 1046.java
@@ -0,0 +1,25 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+       
+        while (maxHeap.size() > 1) {
+            int first = maxHeap.poll();
+            int second = maxHeap.poll();
+
+            if (first != second) {
+                maxHeap.offer(first - second);
+            }
+        }
+
+        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+}
